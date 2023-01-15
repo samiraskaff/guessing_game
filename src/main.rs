11f299b2c4ac26810b732f563426977e25936abc
Sage Skaff <skaffsage@gmail.com>
2023-01-15 09:02:56 -0800

    Add match statement to compare input guess with secret number

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -15,6 +14,16 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("The secret number is: {secret_number}");
 }
